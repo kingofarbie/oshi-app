57e0a77dd3dc404fe45119c51459a18b64812b50
kingofarbie <king.of.arbie@gmail.com>
2026-06-21 12:44:10 +0900

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAw4krU9eVoUQegnxTCS02G_rKFCT9HlA0",
+  authDomain: "oshi-app-737c0.firebaseapp.com",
+  projectId: "oshi-app-737c0",
+  storageBucket: "oshi-app-737c0.firebasestorage.app",
+  messagingSenderId: "545181069543",
+  appId: "1:545181069543:web:33809ce86bedd3b28d7242"
+};
+
+// Firebase初期化
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging =
+    firebase.messaging();
