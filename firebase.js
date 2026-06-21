5761ce93c1f4e8e458a4baed6d456423c7ce55c1
kingofarbie <king.of.arbie@gmail.com>
2026-06-22 01:02:48 +0900

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -41,3 +41,66 @@ const messaging =
     });
 
 }
+
+
+if ('serviceWorker' in navigator) {
+
+    navigator.serviceWorker
+    .register('./firebase-messaging-sw.js')
+
+    .then(function(reg){
+
+        console.log(
+            'Service Worker登録成功'
+        );
+
+    })
+
+    .catch(function(err){
+
+        console.error(
+            'Service Worker登録失敗',
+            err
+        );
+
+    });
+
+}
+
+
+/* =====================
+   FCMトークン取得
+===================== */
+
+messaging.getToken({
+
+    vapidKey:
+    "BN1-_Xmg_3Ghv0gk1sJ_HuQPcl33BoHA8RzMbI0pKC3Shvet0-eLI6WI6MgKI7CQylJy7yCRBgtSpUmwcLPmtn0"
+
+})
+.then((currentToken)=>{
+
+    if(currentToken){
+
+        console.log(
+            "FCMトークン:",
+            currentToken
+        );
+
+    }else{
+
+        console.log(
+            "トークン取得失敗"
+        );
+
+    }
+
+})
+.catch((err)=>{
+
+    console.error(
+        "FCMエラー:",
+        err
+    );
+
+});
