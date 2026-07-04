c6d2a10d9b4cdb749836fb29ac5951d4c89d88a4
kingofarbie <king.of.arbie@gmail.com>
2026-07-04 11:01:38 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "oshi-app-v1";
+
+self.addEventListener("install", event => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => caches.delete(key))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        fetch(event.request, { cache: "no-store" })
+    );
+});
