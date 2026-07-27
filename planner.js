5d3268dd1531f12808b5cde2b58651b7b94fb7c3
kingofarbie <king.of.arbie@gmail.com>
2026-07-28 01:16:20 +0900

    Add files via upload

--- planner.js
+++ planner.js
@@ -7,24 +7,6 @@
 ===================== */
 
 
-let currentPhotoSrc = "";
-let currentPhotoIndex = 0;
-let touchStartX = 0;
-
-let favoriteViewMode = false;
-
-let photoScale = 1;
-let lastDistance = 0;
-
-
-let photoTranslateX = 0;
-let photoTranslateY = 0;
-
-let dragStartX = 0;
-let dragStartY = 0;
-
-let lastTapTime = 0;
-
 
 
 /* =====================
@@ -643,53 +625,6 @@ const photo =
 }
 
 
-function updateFavoriteButton(){
-
-    const btn =
-        document.querySelector(
-            ".photo-favorite"
-        );
-
-
-    if(!btn) return;
-
-
-    const data = db.load();
-
-
-    let photo = null;
-
-
-    Object.values(
-        data.dayMemories || {}
-    )
-    .forEach(day=>{
-
-        (day.photos || [])
-        .forEach(p=>{
-
-            if(p.id === currentPhotoId){
-
-                photo = p;
-
-            }
-
-        });
-
-    });
-
-
-    if(photo && photo.favorite){
-
-        btn.textContent = "⭐";
-
-    }else{
-
-        btn.textContent = "☆";
-
-    }
-
-}
 
 function showPhoto(index){
 
