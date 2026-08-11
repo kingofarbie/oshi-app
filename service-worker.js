43ae0b3899294ad1edc6e95280c94bbb11b94569
kingofarbie <king.of.arbie@gmail.com>
2026-08-11 23:38:32 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,38 @@
-const CACHE_NAME = "oshi-app-v1";
+const CACHE_NAME = "oshi-app-v2";
 
-self.addEventListener("install",event=>{
+self.addEventListener("install", event => {
+
     self.skipWaiting();
+
 });
 
-self.addEventListener("activate",event=>{
+
+self.addEventListener("activate", event => {
+
     event.waitUntil(
-        self.clients.claim()
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+
+            );
+
+        }).then(() => {
+
+            return self.clients.claim();
+
+        })
+
     );
+
 });
 
-self.addEventListener("fetch",event=>{
+
+self.addEventListener("fetch", event => {
 
     event.respondWith(
         fetch(event.request)
@@ -18,18 +40,19 @@ self.addEventListener("fetch",event=>{
 
 });
 
-self.addEventListener("notificationclick",event=>{
+
+self.addEventListener("notificationclick", event => {
 
     event.notification.close();
 
     event.waitUntil(
 
         clients.matchAll({
-            type:"window",
-            includeUncontrolled:true
-        }).then(clientList=>{
+            type: "window",
+            includeUncontrolled: true
+        }).then(clientList => {
 
-            if(clientList.length){
+            if (clientList.length) {
 
                 return clientList[0].focus();
 
