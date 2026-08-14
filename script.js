38b9dd6f9ce3d675311d866fa2b6595a1b5fe87c
kingofarbie <king.of.arbie@gmail.com>
2026-08-14 16:37:06 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -316,6 +316,28 @@ function switchTab(pageId, event, fromCalendar = false){
     /* =====================
        お気に入り
     ===================== */
+    if(pageId === 'favoritePage'){
+
+    setTimeout(() => {
+
+        if(
+            typeof favoritePhotoRender ===
+            'function'
+        ){
+
+            favoritePhotoRender();
+
+        }else{
+
+            console.error(
+                "★ favoritePhotoRender() が見つかりません"
+            );
+
+        }
+
+    }, 50);
+
+}
 
 
 
