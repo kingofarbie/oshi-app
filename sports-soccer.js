e2d0024f00f2d352766da46df6e2f467b11d2c6f
kingofarbie <king.of.arbie@gmail.com>
2026-09-12 00:51:32 +0900

    Add files via upload

--- sports-soccer.js
+++ sports-soccer.js
@@ -4270,26 +4270,15 @@ function deleteSoccerGame(){
        ================================================
     */
 
-if(
-    !confirm(
-        "この試合記録を削除しますか？"
-    )
-){
+    if(
+        !confirm(
+            "この試合記録を削除しますか？"
+        )
+    ){
 
-    /*
-       削除をキャンセルした場合、
-       削除ボタンに残っているフォーカスを解除する
-    */
-
-    if(document.activeElement){
-
-        document.activeElement.blur();
+        return;
 
     }
-
-    return;
-
-}
 
 
     const deletedDate =
